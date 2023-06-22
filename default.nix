9166b8ef4fae4251c698b6f471775a28dd38706e
John Ericson <John.Ericson@Obsidian.Systems>
2023-06-22 14:40:03 -0400

    Pin Nixpkgs

--- default.nix
+++ default.nix
@@ -1,5 +1,8 @@
 let
-  pkgs = import <nixpkgs> {};
+  pkgs = import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/30d3d79b7d3607d56546dd2a6b49e156ba0ec634.tar.gz";
+    sha256 = "0x5j9q1vi00c6kavnjlrwl3yy1xs60c34pkygm49dld2sgws7n0a";
+  }) {};
 
   # Create a derivation which, when built, writes some Nix code to
   # its $out path.
